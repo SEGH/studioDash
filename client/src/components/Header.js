0c1b29717e9d02b786c67e4e35d765b038900b44
Susan Holland <segh@fastmail.com>
2020-12-15 19:10:18 -0500

    Added List and NavLinks

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, List, ListItem, ListItemText } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 export default function Header(props) {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6">
-                    StudioDash
-                </Typography>
+                <NavLink to="/">
+                    <Typography variant="h6">
+                        StudioDash
+                    </Typography>
+                </NavLink>
+
+                <List>
+
+                    <ListItem>
+                        <NavLink to="/articles">
+                            <ListItemText inset>
+                                <Typography color="inherit" variant="title">
+                                    Articles
+                                </Typography>
+                            </ListItemText>
+                        </NavLink>
+                    </ListItem >
+
+                </List>
+
             </Toolbar>
         </AppBar>
     )
