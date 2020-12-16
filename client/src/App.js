58d90ce00d49f0d24fa6cdbeea59c2c35b59b228
Susan Holland <segh@fastmail.com>
2020-12-15 19:09:31 -0500

    Added Articles route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Articles from "./pages/Articles";
 
 function App() {
   return (
     <Router>
       <Header />
       <Route exact path="/" render={(props) => (<Home {...props} />)} />
+      <Route exact path="/articles" render={(props) => (<Articles {...props} />)} />
     </Router>
   );
 }
