4065980e554ecbc76c27cc8baa0f8bcc302ada94
Susan Holland <segh@fastmail.com>
2020-12-16 16:54:09 -0500

    Added Admin page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Articles from "./pages/Articles";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Route exact path="/" render={(props) => (<Home {...props} />)} />
       <Route exact path="/articles" render={(props) => (<Articles {...props} />)} />
+      <Route exact path="/admin" component={Admin} />
     </Router>
   );
 }
