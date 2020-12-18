8adda9111ce0d102a17084046649b72e9641ef4a
Susan Holland <segh@fastmail.com>
2020-12-17 19:00:56 -0500

    Imported connect higher order function to pass mapStateToProps returning articles

--- client/src/pages/Articles.js
+++ client/src/pages/Articles.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { ARTICLES_NAME } from "../customStudio";
+import { connect } from "react-redux";
 
-export default function Articles(props) {
+function Articles(props) {
+    const { articles } = props;
+
     return (
         <main>
-            <h1>Articles</h1>
+            <h1>{ARTICLES_NAME}</h1>
+            {articles.map((article) => {
+                return <div><h2>{article.title}</h2><p>{article.body}</p></div>
+            })}
         </main>
     )
 }
+
+const mapStateToProps = (state) => {
+    return {
+        articles: state.articles
+    }
+}
+
+export default connect(mapStateToProps)(Articles)
